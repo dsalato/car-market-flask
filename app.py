ea35a82559d8916989d742abff7f6474df002776
dsalato <d.salato@mail.ru>
2025-05-01 12:04:09 +0700

    add generate id

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from flask_restplus import Api, Resource, fields
 app = Flask(__name__)
 api = Api(app, title='Car Market API', description='API для рынка автомобилей')
 
-# Основное пространство имён (как name_space в вашем примере)
 ns_car = api.namespace('cars', description='Операции с автомобилями')
 
-# Модель данных для автомобиля (аналог info в part.py)
 car_model = api.model('Car', {
     'id': fields.String(required=True, description='ID автомобиля'),
     'brand': fields.String(required=True, description='Марка (Toyota, BMW)'),
@@ -17,13 +15,12 @@ car_model = api.model('Car', {
     'mileage': fields.Integer(description='Пробег (км)'),
 })
 
-# "База данных" (аналог INFO в part.py)
 CARS = [
     {'id': '1', 'brand': 'Toyota', 'model': 'Camry', 'year': 2020, 'price': 25000, 'mileage': 15000},
     {'id': '2', 'brand': 'BMW', 'model': 'X5', 'year': 2019, 'price': 45000, 'mileage': 30000},
 ]
 
-# Эндпоинты для автомобилей (аналог InfoList и InfoId в part.py)
+
 @ns_car.route('/')
 class CarList(Resource):
     @ns_car.marshal_list_with(car_model)
@@ -36,8 +33,18 @@ class CarList(Resource):
     def post(self):
         """Добавить новый автомобиль"""
         new_car = api.payload
+
+        # Генерация нового ID
+        if CARS:
+            max_id = max(int(car['id']) for car in CARS)
+            new_id = str(max_id + 1)
+        else:
+            new_id = '1'
+
+        new_car['id'] = new_id
         CARS.append(new_car)
         return new_car, 201
+
 
 @ns_car.route('/<string:id>')
 @ns_car.param('id', 'ID автомобиля')
@@ -67,7 +74,7 @@ class CarResource(Resource):
         CARS = [car for car in CARS if car['id'] != id]
         return {'message': 'Автомобиль удален'}, 200
 
-# Подключение дополнительных модулей (как part.py и parttmpl.py)
+
 from car_api.cars import api as cars_ns
 from car_api.templates import templ as car_templ
 
